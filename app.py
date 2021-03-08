2b8b5659d72ab570cee79105a6c9cfe2c8ed9863
Prasanna Kumar Gaddad <prasangaddad@gmail.com>
2021-03-08 15:11:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request, Markup ,redirect
 import numpy as np
 import pandas as pd
-import requests
-import pickle
-import os
 from utils.disease import disease_dic
 from utils.fertilizer import fertilizer_dic
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.models import model_from_json
+import requests
+import pickle
+import io
+import torch
+from torchvision import transforms
+from PIL import Image
+from utils.model import ResNet9
 # =========================================================================================
 
 app = Flask(__name__)
@@ -23,7 +25,7 @@ def home():
 
 # =========================================================================================
 
-crop_recommendation_model_path = 'model/crop_prediction/RF_pkl.pkl'
+crop_recommendation_model_path = 'models/crop_prediction/RF_pkl.pkl'
 crop_recommendation_model = pickle.load(
     open(crop_recommendation_model_path, 'rb'))
 
@@ -71,15 +73,15 @@ def crop_prediction():
         # state = request.form.get("stt")
         city = request.form.get("city")
         
-        try: 
-            if weather_fetch(city) != None:
-               temperature, humidity = weather_fetch(city)
-               data = np.array([[N, P, K, temperature, humidity, ph, rainfall]])
-               my_prediction = crop_recommendation_model.predict(data)
-               final_prediction = my_prediction[0]
-               return render_template('crop-result.html', prediction=final_prediction, title=title)
-        except:
-            return render_template('crop.html', prediction=final_prediction, title=title)
+        
+        if weather_fetch(city) != None:
+            temperature, humidity = weather_fetch(city)
+            data = np.array([[N, P, K, temperature, humidity, ph, rainfall]])
+            my_prediction = crop_recommendation_model.predict(data)
+            final_prediction = my_prediction[0]
+            return render_template('crop-result.html', prediction=final_prediction, title=title)
+        else:
+            return render_template('try_again.html')
             
 
         
@@ -136,36 +138,133 @@ def fert_recommend():
 # ===============================================================================================
 
 
-json_file = open('model/disease classification/model.json' , 'r')
-loaded_model = json_file.read()
+# Loading plant disease classification model
 
-json_file.close()
+disease_classes = ['Apple___Apple_scab',
+                   'Apple___Black_rot',
+                   'Apple___Cedar_apple_rust',
+                   'Apple___healthy',
+                   'Blueberry___healthy',
+                   'Cherry_(including_sour)___Powdery_mildew',
+                   'Cherry_(including_sour)___healthy',
+                   'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+                   'Corn_(maize)___Common_rust_',
+                   'Corn_(maize)___Northern_Leaf_Blight',
+                   'Corn_(maize)___healthy',
+                   'Grape___Black_rot',
+                   'Grape___Esca_(Black_Measles)',
+                   'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+                   'Grape___healthy',
+                   'Orange___Haunglongbing_(Citrus_greening)',
+                   'Peach___Bacterial_spot',
+                   'Peach___healthy',
+                   'Pepper,_bell___Bacterial_spot',
+                   'Pepper,_bell___healthy',
+                   'Potato___Early_blight',
+                   'Potato___Late_blight',
+                   'Potato___healthy',
+                   'Raspberry___healthy',
+                   'Soybean___healthy',
+                   'Squash___Powdery_mildew',
+                   'Strawberry___Leaf_scorch',
+                   'Strawberry___healthy',
+                   'Tomato___Bacterial_spot',
+                   'Tomato___Early_blight',
+                   'Tomato___Late_blight',
+                   'Tomato___Leaf_Mold',
+                   'Tomato___Septoria_leaf_spot',
+                   'Tomato___Spider_mites Two-spotted_spider_mite',
+                   'Tomato___Target_Spot',
+                   'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+                   'Tomato___Tomato_mosaic_virus',
+                   'Tomato___healthy']
 
-model = model_from_json(loaded_model)
-model.load_weights('model/disease prediction/model.h5')
+disease_model_path = 'models/plant_disease_model.pth'
+disease_model = ResNet9(3, len(disease_classes))
+disease_model.load_state_dict(torch.load(
+    disease_model_path, map_location=torch.device('cpu')))
+disease_model.eval()
 
 
-upload_folder = 'static/uploads'
+def predict_image(img, model=disease_model):
+    """
+    Transforms image to tensor and predicts disease label
+    :params: image
+    :return: prediction (string)
+    """
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.ToTensor(),
+    ])
+    image = Image.open(io.BytesIO(img))
+    img_t = transform(image)
+    img_u = torch.unsqueeze(img_t, 0)
+
+    # Get predictions from model
+    yb = model(img_u)
+    # Pick index with highest probability
+    _, preds = torch.max(yb, dim=1)
+    prediction = disease_classes[preds[0].item()]
+    # Retrieve the class label
+    return prediction
+
+
+
+# render disease prediction result page
+
 
 @app.route('/disease-predict', methods=['GET', 'POST'])
 def disease_prediction():
+    title = 'Harvestify - Disease Detection'
+
     if request.method == 'POST':
-        img = request.files['file']
-        if img:
-            
-            image_loc = os.path.join(upload_folder, img.filename)
-            
-            img.save(image_loc)
-            
-            img = image.load_img(image_loc, target_size=(224, 224))
-            x = image.img_to_array(img)
-            x = x/255
-            img_data = np.expand_dims(x, axis=0)
-            a = np.argmax(model.predict(img_data), axis=1)
-    
-            prediction = Markup(str(disease_dic[a[0]]))
-        return render_template('disease-result.html', prediction=prediction)
-    return render_template('dise.html')
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files.get('file')
+        if not file:
+            return render_template('disease.html', title=title)
+        try:
+            img = file.read()
+
+            prediction = predict_image(img)
+
+            prediction = Markup(str(disease_dic[prediction]))
+            return render_template('disease-result.html', prediction=prediction, title=title)
+        except:
+            pass
+    return render_template('dise.html', title=title)
+
+
+#json_file = open('models/disease prediction/model.json' , 'r')
+#loaded_model = json_file.read()
+
+#json_file.close()
+
+#model = model_from_json(loaded_model)
+#model.load_weights('models/disease prediction/model.h5')
+
+
+#upload_folder = 'static/uploads'
+
+#@app.route('/disease-predict', methods=['GET', 'POST'])
+#def disease_prediction():
+#    if request.method == 'POST':
+#        img = request.files['file']
+#        if img:
+#            
+#            image_loc = os.path.join(upload_folder, img.filename)
+#            
+#            img.save(image_loc)
+#            
+#            img = image.load_img(image_loc, target_size=(224, 224))
+#            x = image.img_to_array(img)
+#            x = x/255
+#            img_data = np.expand_dims(x, axis=0)
+#            a = np.argmax(model.predict(img_data), axis=1)
+#    
+#            prediction = Markup(str(disease_dic[a[0]]))
+#        return render_template('disease-result.html', prediction=prediction)
+#    return render_template('dise.html')
 
 # ===============================================================================================
 if __name__ == '__main__':
